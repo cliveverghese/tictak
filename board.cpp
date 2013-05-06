07b084004c6f3b1f47350d2bd23d24d50500df76
Clive Verghese <cliveverghese@gmail.com>
2013-05-06 22:03:34 +0530

    Added a documentation

--- board.cpp
+++ board.cpp
@@ -1,5 +1,19 @@
 #include "board.h"
+#include <cstdio>
 
+
+board::board(){
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 3; j++){	
+			bd[i][j] = ' ';
+		
+		}
+	}
+	#ifdef DEBUG
+		printf("Initilized board with values\n");
+	#endif
+	
+}
 
 int board::make_move(char player, position pos){
 	bd[pos.first][pos.second] = player;
--- board.h
+++ board.h
@@ -12,11 +12,20 @@
 typedef std::pair<int, int> position;
 
 
+/** 
+* Class board. It handles the board functions and maintains the board status
+* after each move.
+**/ 
 
 class board{
 	char bd[3][3];
 	
 	public:
+	/**
+	* An enum
+	* The various states the board can be in at a point of time. CONTD signifies
+	* the game is not yet over.
+	**/
 	enum {
 		WIN_X,
 		WIN_O,
@@ -24,8 +33,25 @@ class board{
 		CONTD
 	};
 	
+	/**
+	* Constructor to initilize all values with a ' '
+	**/
+	board();
+	
+	/**
+	* Make a move on the board. 
+	* @param player The player making the move. This could either be a 'X' or a 'O'
+	* @param pos The position where the player made the move.
+	* @return The current status of the board after the move is made.
+	**/
 	int make_move(char player, position pos);
 	
+	/**
+	* Return the value at a position.
+	* @param pos position whose value is to be returned.
+	* @return The character at the position pos. ' ' if no player has played the
+	* box.
+	**/
 	char get_position(position pos);
 	
 };
