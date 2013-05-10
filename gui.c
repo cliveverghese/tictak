6d80ef3e21bb79a29f0f52558d80e6df30080e85
Pranav Ashok <pranavashok@gmail.com>
2013-05-10 23:44:44 +0530

    Find grid in which click is performed

--- gui.c
+++ gui.c
@@ -22,15 +22,58 @@ void GLFWCALL display(){
 	glEnd();
 	glfwSwapBuffers();
 }
-
-
+int GLFWCALL process_click() {
+	int x, y;
+	if(glfwGetMouseButton(GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+        glfwGetMousePos(&x, &y);
+        if(y < 133) {
+            if(x < 133) {
+                return 1;
+            }
+            else if (x > 266) {
+                return 3;
+            }
+            else {
+                return 2;
+            }
+        }
+        else if(y > 266) {
+            if(x < 133) {
+                return 7;
+            }
+            else if (x > 266) {
+                return 9;
+            }
+            else {
+                return 8;
+            }   
+        }
+        else {
+            if(x < 133) {
+                return 4;
+            }
+            else if (x > 266) {
+                return 6;
+            }
+            else {
+                return 5;
+            }   
+        }
+    }
+}
 void GLFWCALL window_thread(){
+	int grid_id;
 	glfwInit();
 	printf("Running\n");
-	glfwOpenWindow(500,500,8,8,8,0,24,0,GLFW_WINDOW);
+	glfwOpenWindow(400,400,8,8,8,0,24,0,GLFW_WINDOW);
 	glfwSetWindowSizeCallback(reshape);
 	while(glfwGetWindowParam(GLFW_OPENED)){
 		display();
+		grid_id = 0;
+		grid_id = process_click();
+		if(grid_id != 0) {
+			printf("%d\n", grid_id);
+		}
 	}
 	return;
 }
